83584dd74018575ee88e18235bd96291306b4d34
ndungu1mac <macgeorgendungu@gmail.com>
2022-10-10 16:21:45 +0300

    Completed

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
@@ -5,7 +6,10 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
+        isPublished={true}
+        minutesToRead={1} 
+      />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
